451ba537ea9baa71eb03e861712608c17cf0d9ef
NEPTR33 <baiolemi@gmail.com>
2023-09-09 03:06:34 -0400

    first comit

---
+++ whatToDo.js
@@ -0,0 +1,16 @@
+jQuery(function () {
+  $("#input").change(function () {
+    let input = $(this).val();
+    $("ul").append(
+      `<li>${input}<i class="fas fa-check"></i><i class="fas fa-trash"></i></li>`
+    );
+    $(this).val("");
+  });
+
+  $("ul").on("click", ".fa-check", function () {
+    $(this).parent("li").toggleClass("checked");
+  });
+  $("ul").on("click", ".fa-trash", function () {
+    $(this).parent("li").fadeOut(150);
+  });
+});
